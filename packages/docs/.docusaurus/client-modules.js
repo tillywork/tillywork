c366bf1148ee8879428e4c1f17c16791c723051d
Moayyad Shaddad <shaddadmoayyad@gmail.com>
2024-05-29 20:56:32 +0300

    docs: woohoo

---
+++ packages/docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/moshizzle/tillywork/packages/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/moshizzle/tillywork/packages/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/moshizzle/tillywork/packages/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/moshizzle/tillywork/packages/docs/src/css/custom.css"),
+];
