d4159a0d521270ed10caab02c33947916c8b872f
Moayyad Shaddad <shaddadmoayyad@gmail.com>
2024-06-09 17:35:20 +0300

    fix: posthog

--- packages/frontend/src/plugins/posthog.js
+++ packages/frontend/src/plugins/posthog.js
@@ -2,9 +2,9 @@ import posthog from 'posthog-js';
 
 export default {
     install(app) {
-        if (import.meta.env.TW_POSTHOG_KEY) {
+        if (import.meta.env.TW_VITE_POSTHOG_KEY) {
             app.config.globalProperties.$posthog = posthog.init(
-                import.meta.env.TW_POSTHOG_KEY,
+                import.meta.env.TW_VITE_POSTHOG_KEY,
                 {
                     api_host: 'https://us.i.posthog.com',
                 }
