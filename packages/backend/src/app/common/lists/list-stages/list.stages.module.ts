340d36b58af29172840020806484b35322da94f1
achsan <achsanh@gmail.com>
2024-06-19 06:08:17 -0400

    feat(be-list_stage): Implemented event subscriber for updating 'List Group'

--- packages/backend/src/app/common/lists/list-stages/list.stages.module.ts
+++ packages/backend/src/app/common/lists/list-stages/list.stages.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ListStagesController } from "./list.stages.controller";
 import { ListStage } from "./list.stage.entity";
 import { ListStagesService } from "./list.stages.service";
+import { ListStagesSubscriber } from "./list.stages.subscriber";
 import { CardListsModule } from "../../cards/card-lists/card.lists.module";
 
 @Module({
     imports: [TypeOrmModule.forFeature([ListStage]), CardListsModule],
     controllers: [ListStagesController],
-    providers: [ListStagesService],
+    providers: [ListStagesService, ListStagesSubscriber],
     exports: [ListStagesService],
 })
 export class ListStagesModule {}
