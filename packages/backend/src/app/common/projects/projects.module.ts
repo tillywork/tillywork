9b83a1729d3c49e530b602a8dfea20c97429cde5
Moayyad Shaddad <shaddadmoayyad@gmail.com>
2024-07-01 17:21:05 +0300

    chore: remove project invite codes module

--- packages/backend/src/app/common/projects/projects.module.ts
+++ packages/backend/src/app/common/projects/projects.module.ts
@@ -4,14 +4,9 @@ import { ProjectsController } from "./projects.controller";
 import { ProjectsService } from "./projects.service";
 import { Project } from "./project.entity";
 import { ProjectUsersModule } from "./project-users/project.users.module";
-import { ProjectInviteCodesModule } from "./project-invite-codes/project.invite.codes.module";
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([Project]),
-        ProjectUsersModule,
-        ProjectInviteCodesModule,
-    ],
+    imports: [TypeOrmModule.forFeature([Project]), ProjectUsersModule],
     controllers: [ProjectsController],
     providers: [ProjectsService],
     exports: [ProjectsService],
