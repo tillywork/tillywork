23cc853d75ae000ba7831cee8383325e978c73c9
achsan <achsanh@gmail.com>
2024-07-19 04:10:49 -0400

    feat(fe-base): Register baseContextMenu component

---
+++ packages/frontend/src/components/common/base/BaseContextMenu.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+const props = defineProps<{
+  elementId: string;
+  items: readonly any[];
+}>();
+
+const emit = defineEmits(['context-menu:click:action', 'context-menu:close']);
+
+const toggleComponent = ref<boolean>();
+const componentDimension = reactive({
+  width: 0,
+  height: 0,
+});
+
+// Ref: https://github.com/johndatserakis/vue-simple-context-menu/blob/develop/src/vue-simple-context-menu.vue
+const data = ref<any>();
+function showMenu(event: MouseEvent, context: any) {
+  toggleComponent.value = true;
+  data.value = context;
+
+  let menu = document.getElementById(props.elementId);
+  if (!menu) {
+    return;
+  }
+
+  if (!componentDimension.width || !componentDimension.height) {
+    componentDimension.width = menu.offsetWidth;
+    componentDimension.height = menu.offsetHeight;
+  }
+
+  const borderSize = 2;
+  const navigationWidth = 256;
+  // const topBarHeight = 40;
+
+  // TODO: Dynamic Menu Position
+
+  // const isPlaceMenuOnRight =
+  //   navigationWidth + componentDimension.width + event.pageX >= window.innerWidth;
+  // if (isPlaceMenuOnRight) {
+  //   menu.style.left =
+  //     event.pageX - componentDimension.width + borderSize + 'px';
+  //   menu.style.left =
+  //     event.pageX - navigationWidth - componentDimension.width + borderSize + 'px'; // If navigationDrawer included
+  // } else {
+  //   menu.style.left = event.pageX + borderSize + 'px';
+  //   menu.style.left = event.pageX - navigationWidth + borderSize + 'px'; // If navigationDrawer included
+  // }
+
+  // const isPlaceMenuOnBottom =
+  //   topBarHeight + componentDimension.height + event.pageY >= window.innerHeight;
+  // if (isPlaceMenuOnBottom) {
+  //   menu.style.top =
+  //     event.pageY - componentDimension.height + borderSize + 'px';
+  //   menu.style.top =
+  //     event.pageY - topBarHeight - componentDimension.height + borderSize + 'px'; // If appBar included
+  // } else {
+  //   menu.style.top = event.pageY - topBarHeight - borderSize + 'px'; // If appBar included
+  //   menu.style.top = event.pageY - borderSize + 'px';
+  // }
+
+  menu.style.left = event.pageX - navigationWidth + borderSize + 'px';
+  menu.style.top = event.pageY - borderSize + 'px';
+}
+
+function handleAction(action: {
+  id: unknown;
+  value: boolean;
+  path: unknown[];
+}) {
+  hideContextMenu();
+  emit('context-menu:click:action', { action, data: data.value });
+}
+
+function hideContextMenu() {
+  toggleComponent.value = false;
+  const element = document.getElementById(props.elementId);
+  if (element) {
+    emit('context-menu:close');
+  }
+}
+
+function onClickOutside() {
+  hideContextMenu();
+}
+
+function onEscKeyRelease(event: KeyboardEvent) {
+  if (event.key === 'Escape') {
+    hideContextMenu();
+  }
+}
+
+onMounted(() => document.body.addEventListener('keyup', onEscKeyRelease));
+onBeforeUnmount(() => document.body.addEventListener('keyup', onEscKeyRelease));
+
+defineExpose({ showMenu });
+</script>
+
+<template>
+  <v-sheet
+    :id="elementId"
+    border
+    class="position-absolute"
+    :class="{ 'd-none': !toggleComponent }"
+    v-click-outside="{ handler: onClickOutside }"
+  >
+    <v-list
+      width="200"
+      :items="items"
+      @click:select="handleAction"
+      bg-color="white"
+      base-color="black"
+    />
+  </v-sheet>
+</template>
