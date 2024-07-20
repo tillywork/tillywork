731e0075dae118483a92586d8a4b0642b464d9f3
achsan <achsanh@gmail.com>
2024-07-20 03:04:58 -0400

    feat(fe-base): Add emitter event to update items in BaseLabelSelector

--- packages/frontend/src/components/common/inputs/BaseLabelSelector.vue
+++ packages/frontend/src/components/common/inputs/BaseLabelSelector.vue
@@ -1,59 +1,90 @@
 <script setup lang="ts">
 import type { FieldItem } from '@/components/project-management/fields/types';
 
-const selected = defineModel<string[]>({
-  default: [],
-});
-
-const selectedItems = computed(() =>
-  selected.value?.map((label) =>
-    props.items?.find((item) => item.item === label)
-  )
-);
-
-const searchLabels = ref<string>();
-const searchedLabels = computed(() => {
-  if (searchLabels.value) {
-    return props.items?.filter((item) =>
-      item.item
-        .toLocaleLowerCase()
-        .includes(searchLabels.value!.toLocaleLowerCase())
-    );
-  }
-
-  return props.items;
-});
-
-const props = defineProps<{
+interface Props {
   items?: FieldItem[];
   placeholder?: string;
   multiple?: boolean;
   icon?: string;
   textField?: boolean;
-}>();
-
-function isItemSelected(item: FieldItem) {
-  return !!selected.value?.find((label) => item.item === label);
 }
+const props = withDefaults(defineProps<Props>(), {
+  items: () => [],
+});
+const emit = defineEmits(['label:update:items']);
+
+const labelValue = defineModel<string | string[]>({ default: [] });
+
+const itemsCopy = ref<FieldItem[]>(props.items);
+
+watch(labelValue, (v) => {
+  const items = props.items.map((item) => item.item);
+  if (Array.isArray(v)) {
+    if (v.some((label) => !items.includes(label))) {
+      emit('label:update:items', itemsCopy.value);
+    }
+  } else {
+    if (!items.includes(v)) {
+      emit('label:update:items', itemsCopy.value);
+    }
+  }
+});
+
+function handleSearch(search: string) {
+  if (search) {
+    itemsCopy.value = appendItemsWithSearch(props.items, search);
+  }
+}
+
+function appendItemsWithSearch(items: FieldItem[], search: string) {
+  const item = items.find((item) => item.item === search);
+
+  if (item) {
+    return items;
+  } else {
+    return [{ item: search, color: 'default' }, ...items];
+  }
+}
+
+// !TextField
+const searchText = ref<string>('');
+const searchResult = computed(() => {
+  handleSearch(searchText.value);
+
+  if (searchText.value) {
+    return appendItemsWithSearch(itemsCopy.value, searchText.value);
+  } else {
+    return itemsCopy.value;
+  }
+});
+
+const selectedItems = computed(() => {
+  if (Array.isArray(labelValue.value)) {
+    return labelValue.value
+      .map((label) => itemsCopy.value.find((item) => item.item === label))
+      .filter(Boolean) as FieldItem[];
+  } else {
+    return [
+      itemsCopy.value.find((item) => item.item === labelValue.value),
+    ].filter(Boolean) as FieldItem[];
+  }
+});
 
 function toggleItemSelection(item: FieldItem) {
   if (props.multiple) {
-    if (!isItemSelected(item)) {
-      selected.value = [...selected.value, item.item];
-    } else {
-      const index = selected.value.findIndex((label) => label === item.item);
-
-      selected.value = [
-        ...selected.value.slice(0, index),
-        ...selected.value.slice(index + 1),
+    if (selectedItems.value.map((item) => item.item).includes(item.item)) {
+      const index = (labelValue.value as string[]).findIndex(
+        (label) => label === item.item
+      );
+      labelValue.value = [
+        ...labelValue.value.slice(0, index),
+        ...labelValue.value.slice(index + 1),
       ];
+    } else {
+      labelValue.value = [...labelValue.value, item.item];
     }
   } else {
-    if (!isItemSelected(item)) {
-      selected.value = [item.item];
-    } else {
-      selected.value = [];
-    }
+    labelValue.value = item.item;
   }
 }
 </script>
@@ -61,8 +92,8 @@ function toggleItemSelection(item: FieldItem) {
 <template>
   <template v-if="textField">
     <v-autocomplete
-      v-model="selected"
-      :items
+      v-model="labelValue"
+      :items="itemsCopy"
       item-title="item"
       item-value="item"
       hide-details
@@ -72,6 +103,7 @@ function toggleItemSelection(item: FieldItem) {
       autocomplete="off"
       chips
       auto-select-first
+      @update:search="handleSearch"
     >
       <template #chip="{ item, props }">
         <v-chip v-bind="props" :color="item.raw.color" variant="flat" />
@@ -81,14 +113,14 @@ function toggleItemSelection(item: FieldItem) {
   <template v-else>
     <v-menu
       :close-on-content-click="false"
-      @update:model-value="searchLabels = ''"
+      @update:model-value="searchText = ''"
     >
       <template #activator="{ props }">
         <div v-bind="props" class="d-flex ga-1">
           <template v-if="selectedItems.length">
             <template v-for="item in selectedItems" :key="item.item">
-              <v-chip :color="item?.color" variant="flat" link size="small">
-                {{ item?.item }}
+              <v-chip :color="item.color" variant="flat" link size="small">
+                {{ item.item }}
               </v-chip>
             </template>
           </template>
@@ -99,7 +131,7 @@ function toggleItemSelection(item: FieldItem) {
       </template>
       <v-card>
         <v-text-field
-          v-model="searchLabels"
+          v-model="searchText"
           placeholder="Search.."
           autofocus
           hide-details
@@ -107,9 +139,9 @@ function toggleItemSelection(item: FieldItem) {
           autocomplete="off"
         />
         <v-list>
-          <template v-for="item in searchedLabels" :key="item.item">
+          <template v-for="item in searchResult" :key="item.item">
             <v-list-item
-              :active="isItemSelected(item)"
+              :active="selectedItems.includes(item)"
               @click="toggleItemSelection(item)"
             >
               <v-chip :color="item.color" variant="flat" size="small">
@@ -119,7 +151,7 @@ function toggleItemSelection(item: FieldItem) {
                 <v-icon
                   icon="mdi-check"
                   size="12"
-                  v-if="isItemSelected(item)"
+                  v-if="selectedItems.includes(item)"
                 />
               </template>
             </v-list-item>
