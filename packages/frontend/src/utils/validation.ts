f3332eeb13ced43361a7714a0cfae7dd72bec20d
Dakshesh Jain <dakshesh.jain14@gmail.com>
2025-05-17 21:48:08 +0530

    feat: made function to validate password depending upon the policy

--- packages/frontend/src/utils/validation.ts
+++ packages/frontend/src/utils/validation.ts
@@ -1,3 +1,29 @@
+const passwordPolicy =
+  import.meta.env.TW_VITE_PASSWORD_VALIDATION_POLICY || 'none';
+
+const getPasswordRule = () => {
+  switch (passwordPolicy) {
+    case 'very_strict':
+      return (v: string) =>
+        !v ||
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,}$/.test(v) ||
+        'Password must be at least 12 characters, include uppercase, lowercase, number, and special character';
+    case 'strict':
+      return (v: string) =>
+        !v ||
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(v) ||
+        'Password must be at least 8 characters, include uppercase, lowercase, and number';
+    case 'normal':
+      return (v: string) =>
+        !v ||
+        /^.{6,}$/.test(v) ||
+        'Password must be at least 6 characters long';
+    case 'none':
+    default:
+      return () => true;
+  }
+};
+
 const validationUtils = {
   rules: {
     required: (v: any) =>
@@ -14,6 +40,7 @@ const validationUtils = {
       !v ||
       /^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-./?%&=]*)?$/i.test(v) ||
       'Invalid URL format',
+    password: getPasswordRule(),
   },
 };
 
